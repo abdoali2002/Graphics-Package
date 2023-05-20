061a2d2f3bc502495be695f46902819aaa891c70
Abdelrahman-Ali <74362044+abdoali2002@users.noreply.github.com>
2023-05-20 14:27:23 +0300

    Create point.cs

---
+++ Gr/Gr/Graphics package/point.cs
@@ -0,0 +1,38 @@
+﻿namespace Gr.Graphics_package
+{
+    internal class point
+    {
+        private int xPoint;
+        private int yPoint;
+        private int zPoint;
+
+        public point(int xPoint, int yPoint)
+        {
+            this.xPoint = xPoint;
+            this.yPoint = yPoint;
+        }
+        public point(int xPoint, int yPoint, int zPoint)
+        {
+            this.xPoint = xPoint;
+            this.yPoint = yPoint;
+            this.zPoint = zPoint;
+        }
+
+        public int GetXPoint()
+        {
+            return this.xPoint;
+        }
+
+        public int GetYPoint()
+        {
+            return this.yPoint;
+        }
+
+        public int GetZPoint()
+        {
+            return this.zPoint;
+        }
+
+
+    }
+}
