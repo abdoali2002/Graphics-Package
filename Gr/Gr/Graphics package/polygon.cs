ae8c14289631409f11f5669fa51f0bf80d2adc6a
Abdelrahman-Ali <74362044+abdoali2002@users.noreply.github.com>
2023-05-20 14:27:29 +0300

    Create polygon.cs

---
+++ Gr/Gr/Graphics package/polygon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Gr.Graphics_package
+{
+    internal class polygon
+    {
+        private List<point> points;
+        public polygon(List<point> points)
+        {
+            this.points = points;
+        }
+
+        public List<point> GetPoints()
+        {
+            return points;
+        }
+    }
+}
